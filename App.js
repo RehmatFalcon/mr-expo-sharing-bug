a2d0115cd10625a94b04ef9031d641dd6f253f1f
Kushal Niroula <niroulakushal31@gmail.com>
2023-03-30 12:31:14 +0545

    chore: exp

--- App.js
+++ App.js
@@ -26,7 +26,6 @@ export default function App() {
     await Sharing.shareAsync(uri, {
       UTI:".pdf",
       dialogTitle:"SHare ITem",
-      mimeType:"application/pdf"
     });
     
     console.log("After sharing")
